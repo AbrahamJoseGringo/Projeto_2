328c7b99d765cc2bbe68d5469daa72179de8f390
AbrahamJoseGringo <aperez.castro32@gmail.com>
2023-05-05 16:21:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+const produtos = [
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90
+    }
+]
+
+</script>
+<template>
+ <input type="text" v-model="novoItem">
+  <button @click="adicionar">Adicionar</button>
+  <ul>
+    <li v-for="item in listaCompras">{{ item }}</li>
+  </ul>
+ <p></p>
+</template>
