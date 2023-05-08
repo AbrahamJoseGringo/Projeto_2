5c75fa482534d5b7120d2c398caf4e28a57e003b
AbrahamJoseGringo <aperez.castro32@gmail.com>
2023-05-08 11:08:32 -0300

    f

--- src/App.vue
+++ src/App.vue
@@ -55,15 +55,82 @@ const produtos = [
     }
 ]
 
+export default {
+  data() {
+    return {
+      carrinho: {
+        items: [],
+        total: 0
+      },
+      produtos
+    };
+  },
+  methods: {
+    adicionarProduto(produto) {
+      let itemCarrinho = this.carrinho.items.find(item => item.id === produto.id);
+
+      if (itemCarrinho) {
+        itemCarrinho.quantidade++;
+        itemCarrinho.valorTotal = itemCarrinho.preco * itemCarrinho.quantidade;
+      } else {
+        itemCarrinho = {
+          ...produto,
+          quantidade: 1,
+          valorTotal: produto.preco
+        };
+        this.carrinho.items.push(itemCarrinho);
+      }
+
+      this.carrinho.total += produto.preco;
+    },
+    incrementarQuantidade(index) {
+      const item = this.carrinho.items[index];
+      item.quantidade++;
+      item.valorTotal = item.preco * item.quantidade;
+      this.carrinho.total += item.preco;
+    },
+    decrement(item) {
+  const index = this.carrinho.items.findIndex((i) => i.id === item.id);
+
+  if (this.carrinho.items[index].quantidade > 1) {
+    this.carrinho.items[index].quantidade--;
+    this.carrinho.items[index].valorTotal = this.carrinho.items[index].preco * this.carrinho.items[index].quantidade;
+    this.carrinho.total -= this.carrinho.items[index].preco;} 
+    else {
+    this.remove(item);}
+}
+  }
+  
+
 </script>
 <template>
-   <ul>
-    <li v-for="item in produtos" :key="item.id">
-      <p>{{ item.nome }}</p>
-      <p>R$ {{ item.preco }}</p>
-      <p> {{  }}</p>
-    </li>
-  </ul>
+  <div>
+    <h2>Produtos</h2>
+    <ul>
+      <li v-for="produto in produtos" :key="produto.id">
+        {{ produto.nome }} - R$ {{ produto.preco.toFixed(2) }}
+        <button @click="adicionarProduto(produto)">Adicionar ao carrinho</button>
+      </li>
+    </ul>
 
+    <h2>Carrinho</h2>
+    <ul>
+      <li v-for="(item, index) in carrinho.items" :key="item.id">
+        {{ item.nome }} - R$ {{ item.preco.toFixed(2) }}
+        <button @click="removerProduto(index)">Remover</button>
+        <div>
+          Quantidade: {{ item.quantidade }}
+          <button @click="incrementarQuantidade(index)">+</button>
+          <button @click="decrementarQuantidade(index)">-</button>
+        </div>
+        <div>
+          Valor total: R$ {{ item.valorTotal.toFixed(2) }}
+        </div>
+      </li>
+    </ul>
 
+    <div>
+      <h3>Total: R$ {{ carrinho.total.toFixed(2) }}</h3>
+    </div>
+  </div>
 </template>
