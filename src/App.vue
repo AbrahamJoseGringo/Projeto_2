d126d21eafb17839551158d3535efbf195902d1e
AbrahamJoseGringo <aperez.castro32@gmail.com>
2023-05-12 16:38:41 -0300

    s

--- src/App.vue
+++ src/App.vue
@@ -53,90 +53,49 @@ const produtos = [
     }
 ]
 
-export default {
-    const carrinho= []:
-  data() {
-    return {
-     carrinho: {
-        items: [],
-        total: 0
-      },
-      produtos
-  }
-  methods: {
-    adicionarProduto(produtos) {
-      const itemCarrinho = this.carrinho.items.find(item => item.id === produtos.id);
-     
-      if (itemCarrinho) {
-        itemCarrinho.quantidade++;
-        itemCarrinho.valorTotal = itemCarrinho.preco * itemCarrinho.quantidade;
-      } 
-      else {
-        const novoItem = {
-          produtos,
-          quantidade: 1,
-          valorTotal: produtos.preco
-        };
-        this.carrinho.items.push(novoItem);
-      }
-
-      this.carrinho.total += produto.preco;
-    }
-    incrementarQuantidade(index) {
-      const item = this.carrinho.items[index];
-      item.quantidade++;
-      item.valorTotal = item.preco * item.quantidade;
-      this.carrinho.total += item.preco;
-    },
-    decrement(item) {
-      const index = this.carrinho.items.findIndex(i => i.id === item.id);
-
-      if (this.carrinho.items[index].quantidade > 1) {
-        this.carrinho.items[index].quantidade--;
-        this.carrinho.items[index].valorTotal = this.carrinho.items[index].preco * this.carrinho.items[index].quantidade;
-        this.carrinho.total -= this.carrinho.items[index].preco;
-      } else {
-        this.remove(item);
-      }
-    },
-    remove(item) {
-      const index = this.carrinho.items.findIndex(i => i.id === item.id);
-      const quantidade = this.carrinho.items[index].quantidade;
-      this.carrinho.items.splice(index, 1);
-      this.carrinho.total -= item.preco * quantidade;
-    }
-  }
-};
-
 </script>
 <template>
-  <div>
-    <h2>Produtos</h2>
-    <ul>
-      <li v-for="produto in produtos" :key="produto.id">
-        {{ produto.nome }} - R$ {{ produto.preco.toFixed(2) }}
-        <button @click="adicionarProduto(produto)">Adicionar ao carrinho</button>
-      </li>
-    </ul>
-
-    <h2>Carrinho</h2>
-    <ul>
-      <li v-for="(item, index) in carrinho.items" :key="item.id">
-        {{ item.nome }} - R$ {{ item.preco.toFixed(2) }}
-        <button @click="removerProduto(index)">Remover</button>
-        <div>
-          Quantidade: {{ item.quantidade }}
-          <button @click="incrementarQuantidade(index)">+</button>
-          <button @click="decrementarQuantidade(index)">-</button>
-        </div>
-        <div>
-          Valor total: R$ {{ item.valorTotal.toFixed(2) }}
-        </div>
-      </li>
-    </ul>
-
-    <div>
-      <h3>Total: R$ {{ carrinho.total.toFixed(2) }}</h3>
-    </div>
-  </div>
+  <ul>
+    <li v-for="item in produtos" :key="item.id">
+      <p>{{ item.nome }}</p> 
+      
+      
+    </li>
+  </ul>
+  <ul>
+    <li v-for="item in produtos" :key="item.id">
+      
+      <p>R${{ item.preco}} </p> 
+      
+    </li>
+  </ul>
 </template>
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  margin-bottom: 8px;
+}
+
+li p {
+  margin: 0;
+}
+
+li p:first-of-type {
+  font-weight: bold;
+}
+
+li p:last-of-type {
+  color: #007bff;
+}
+</style>
